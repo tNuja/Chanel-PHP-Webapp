b5cf3bcbc6a1642e678b7ad317ae52720b1d050a
tNuja <36510738+tNuja@users.noreply.github.com>
2018-03-11 09:10:46 +0530

    Update constDatabase.php

--- includes/constDatabase.php
+++ includes/constDatabase.php
@@ -1,7 +1,13 @@
 <?php
 // Database Constants
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'chaneldb');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+define('DB_HOST', $server);
+define('DB_USER', $username);
+define('DB_PASSWORD', $password);
+define('DB_NAME', $db);
 ?>
