11939df5b9184424a561f50f8a257e99567a6afd
tNuja <36510738+tNuja@users.noreply.github.com>
2018-03-11 13:30:25 +0530

    Update login.php

--- login.php
+++ login.php
@@ -84,7 +84,7 @@ span.psw {
   </div>
 
   <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
+   <button type="button" class="cancelbtn"><a href="index.html" style="color:white">Cancel</a></button>
     <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
 </form>
